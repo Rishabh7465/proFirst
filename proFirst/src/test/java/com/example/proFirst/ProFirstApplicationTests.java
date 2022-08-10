7dac32ccfd76dbdc1976c3207aa04edb3dc3d268
Rishabh Sharma <rishabh.2023mca1061@kiet.edu>
2022-08-10 13:55:11 +0530

    this is commit

---
+++ proFirst/src/test/java/com/example/proFirst/ProFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
